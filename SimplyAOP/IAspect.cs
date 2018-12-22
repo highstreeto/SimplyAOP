82585bba925c50604ede51c0df80d6202663a479
Martin Hochstrasser <martin.hochstrasser@gmx.at>
2018-12-22 16:39:30 +0100

    Add summary comments to classes and interfaces

--- SimplyAOP/IAspect.cs
+++ SimplyAOP/IAspect.cs
@@ -2,11 +2,19 @@
 
 namespace SimplyAOP
 {
+    /// <summary>
+    /// <para>Root interface of all aspects</para>
+    /// <para>Terminology is loosely-based on Spring's AOP</para>
+    /// </summary>
     public interface IAspect
     {
         string Name { get; }
     }
 
+    /// <summary>
+    /// <para>A advice which is executed before the target method</para>
+    /// <para>Can alter the parameter of the method</para>
+    /// </summary>
     public interface IBeforeAdvice : IAspect
     {
         void Before(Invocation invocation);
@@ -14,6 +22,10 @@ namespace SimplyAOP
         void Before<TParam>(Invocation invocation, ref TParam parameter);
     }
 
+    /// <summary>
+    /// <para>A advice which is executed after the target method</para>
+    /// <para>Can alter the result and the exception if one is thrown</para>
+    /// </summary>
     public interface IAfterAdvice : IAspect
     {
         void AfterReturning(Invocation invocation);
