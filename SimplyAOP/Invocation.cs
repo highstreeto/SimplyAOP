12056eb04713f7fdd1cf2a568f4e2c394a55076a
Martin Hochstrasser <martin.hochstrasser@gmx.at>
2018-12-22 16:08:30 +0100

    Move Invocation into own file

---
+++ SimplyAOP/Invocation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+
+namespace SimplyAOP
+{
+    public struct Invocation
+    {
+        private readonly Lazy<Type> targetType;
+        private readonly Lazy<MethodInfo> method;
+
+        public Invocation(Lazy<Type> targetType, string methodName)
+        {
+            MethodName = methodName;
+            this.targetType = targetType;
+
+            method = new Lazy<MethodInfo>(() => targetType.Value.GetMethod(methodName));
+        }
+
+        public Type TargetType => targetType.Value;
+
+        public string MethodName { get; }
+    }
+}
