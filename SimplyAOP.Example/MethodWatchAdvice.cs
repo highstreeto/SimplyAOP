8730694176bd4e09d94d72e4f166f5ac5c306e3a
Martin Hochstrasser <martin.hochstrasser@gmx.at>
2019-01-19 13:47:05 +0100

    Add example project for using AspectWeaver

---
+++ SimplyAOP.Example/MethodWatchAdvice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimplyAOP.Example
+{
+    public class MethodConsoleTraceAdvice : IBeforeAdvice, IAfterAdvice
+    {
+        public string Name => "Method Trace";
+
+        public void Before(Invocation invocation)
+            => Console.WriteLine($"Method {invocation.MethodName}() begin");
+        public void Before<TParam>(Invocation invocation, ref TParam parameter) 
+            => Console.WriteLine($"Method {invocation.MethodName}({parameter}) begin");
+
+        public void AfterReturning(Invocation invocation)
+            => Console.WriteLine($"Method {invocation.MethodName}() end");
+        public void AfterReturning<TResult>(Invocation invocation, ref TResult result)
+            => Console.WriteLine($"Method {invocation.MethodName}() = {result} end");
+        public void AfterThrowing(Invocation invocation, ref Exception exception)
+            => Console.WriteLine($"Method {invocation.MethodName}() ! {exception} end");
+    }
+}
