e42c2afddb86d619cc560886ebdde85e61d59297
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-01-19 13:47:05 +0100

    Add example project for using AspectWeaver

---
+++ SimplyAOP.Example/LongRunningService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimplyAOP.Example
+{
+    public interface ILongRunningService
+    {
+        void Execute(int n); 
+    }
+
+    public class LongRunningService : AspectWeaver.Class, ILongRunningService
+    {
+        public LongRunningService(AspectConfiguration config)
+            : base(config) { }
+
+        public void Execute(int n_)
+        {
+            Advice(n_, n => {
+                Console.Write("Crunching numbers ... ");
+                var sleepTime = TimeSpan.FromMilliseconds(
+                    new Random().Next(n * 500)
+                );
+                Thread.Sleep(sleepTime);
+                Console.WriteLine("Done!");
+            });
+        }
+    }
+}
---
+++ SimplyAOP.Example/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SimplyAOP.Example
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new AspectConfiguration();
+            config.AddAspect<MethodConsoleTraceAdvice>();
+
+            ILongRunningService service = new LongRunningService(config);
+
+            service.Execute(n: 10);
+            service.Execute(n: 1);
+        }
+    }
+}
