1a9e7c8d24e220ba0e201c739db10b48889ec443
Martin Hochstrasser <martin.hochstrasser@gmx.at>
2019-02-11 19:00:51 +0100

    Add TransactionalAdvice to example

---
+++ SimplyAOP.Example/TramactionalAdvice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Transactions;
+
+namespace SimplyAOP.Example
+{
+    public class TranslationalAdvice : IBeforeAdvice, IAfterAdvice
+    {
+        public string Name => "Translational (Ambient)";
+
+        public void Before(Invocation invocation)
+            => BeginTransaction(invocation);
+        public void Before<TParam>(Invocation invocation, ref TParam parameter)
+            => BeginTransaction(invocation);
+
+        public void AfterReturning(Invocation invocation)
+            => EndTransaction(invocation, rollback: false);
+        public void AfterReturning<TResult>(Invocation invocation, ref TResult result)
+            => EndTransaction(invocation, rollback: false);
+        public void AfterThrowing(Invocation invocation, ref Exception exception)
+            => EndTransaction(invocation, rollback: true);
+
+        private void BeginTransaction(Invocation invocation)
+        {
+            var scope = new TransactionScope();
+            invocation["txScope"] = scope;
+            Console.WriteLine(" TX Start");
+        }
+
+        private void EndTransaction(Invocation invocation, bool rollback)
+        {
+            var scope = (TransactionScope)invocation["txScope"];
+            if (!rollback)
+            {
+                Console.WriteLine(" TX Complete");
+                scope.Complete();
+            }
+            else
+            {
+                Console.WriteLine(" TX Rollback");
+            }
+            scope.Dispose();
+        }
+    }
+}
