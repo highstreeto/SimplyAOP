4d7d71aa0af96415f8c974adeafad064bf21a614
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-02-11 19:52:44 +0100

    Add a basic fuzz test to Example as BeforeAdvice
    As a BeforeAdvice can modify the parameter use this to randomly set the
    parameters. NumericService.Sum must use req instead of a and b directly
    for fuzzing to take effect.

---
+++ SimplyAOP.Example/FuzzerAdvice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace SimplyAOP.Example
+{
+    public class FuzzerAdvice : IBeforeAdvice
+    {
+        private Random random;
+
+        public FuzzerAdvice() {
+            random = new Random();
+        }
+
+        public string Name => "Fuzz Testing (Parameters are reset by this!)";
+
+        public void Before(Invocation invocation) {
+            // NOP
+        }
+
+        public void Before<TParam>(Invocation invocation, ref TParam parameter) {
+            if (parameter is int) {
+                parameter = (TParam)(object)random.Next(int.MinValue, int.MaxValue);
+            }
+            if (parameter is ValueTuple<int, int> req) {
+                parameter = (TParam)(object)(
+                    random.Next(int.MinValue, int.MaxValue),
+                    random.Next(int.MinValue, int.MaxValue)
+                );
+            }
+        }
+    }
+}
