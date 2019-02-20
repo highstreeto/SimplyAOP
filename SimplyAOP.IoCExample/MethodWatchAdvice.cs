2ff10f12486285f649c55cf3da41bdd21063db14
Martin Hochstrasser <highstreeto@users.noreply.github.com>
2019-02-20 15:04:36 +0100

    Add a example project using IoC (DryIoc)
    Uses DryIoc in combination with aspects. Aspects include deps. to
    services resolved by IoC. Also analyzes the short git commit messages
    of this repo. which is quite cool :sunglasses:

---
+++ SimplyAOP.IoCExample/MethodWatchAdvice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace SimplyAOP.IoCExample
+{
+    public class MethodWatchAdvice : IBeforeAdvice, IAfterAdvice
+    {
+        public string Name => "Method Watch";
+
+        public void Before<TParam, TResult>(Invocation<TParam, TResult> invocation) {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            invocation["watch"] = stopwatch;
+        }
+
+        public void AfterReturning<TParam, TResult>(Invocation<TParam, TResult> invocation)
+            => StopWatch(invocation);
+        public void AfterThrowing<TParam, TResult>(Invocation<TParam, TResult> invocation, ref Exception exception)
+            => StopWatch(invocation);
+
+        private void StopWatch<TParam, TResult>(Invocation<TParam, TResult> invocation) {
+            var stopwatch = (Stopwatch)invocation["watch"];
+            stopwatch.Stop();
+            Console.WriteLine($"{invocation.MethodName}(...) took {stopwatch.Elapsed}");
+        }
+    }
+}
---
+++ SimplyAOP.IoCExample/Model/Event.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimplyAOP.IoCExample.Model
+{
+    public class Event
+    {
+        public Event(string what)
+            : this(DateTime.Now, what, Environment.UserName) { }
+
+        public Event(DateTime when, string what, string who) {
+            When = when;
+            What = what;
+            Who = who;
+        }
+
+        public DateTime When { get; }
+        public string What { get; }
+        public string Who { get; }
+
+        public override string ToString() {
+            return $"[{When}]: <{Who}> {What}";
+        }
+    }
+}
